3985646381ca9c4f4ddc37690f309bcef1f0842d
maksluczak <maksluczak99@gmail.com>
2025-05-20 18:06:05 +0200

    auth router

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/login', authController.handleLogin);
-router.post('/register', authController.handleLogin);
+router.post('/register', authController.handleNewUser);
 
 module.exports = router;
